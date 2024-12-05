5f20a287475848f919bfee861121b4299324106c
Zoltan <arvind12786@gmail.com>
2024-12-05 18:11:11 +0800

    Update parsing-logs1.py

--- parsing-logs1.py
+++ parsing-logs1.py
@@ -17,21 +17,15 @@ denyentries = []
 
 for line in logdata:
     
-    if re.search("DENY", line):
-        #print(line.strip())
+    if re.search("DENY", line):   
         parts = line.split()
-        #action = parts[-1]
-        #print(parts)
         timestampstr = f"{parts[0]} {parts[1]}"
         timestamp = datetime.strptime(timestampstr, '%Y-%m-%d %H:%M:%S')
         source_ips = parts[2]
         denyentries.append((timestamp, source_ips))
-        #print(denyentries)
 
         for i in range(len(denyentries)):
             for j in range(i+1, len(denyentries)):
-                #print(denyentries[i][0])
-                #print(denyentries[j][0])
                 if denyentries[j][0] <= denyentries[i][0] + timedelta(minutes=10):
                     print(f"DENY entry from source IP: {denyentries[i][1]} and & {denyentries[j][1]} are within 10 minutes")
                 
